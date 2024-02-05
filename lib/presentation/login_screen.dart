7149c7d7a5966d9beb833f27a2edadc5889eb8d7
“Mirza” <mirza_azmathullah@cognerium.com>
2024-02-05 17:21:36 +0530

    added output

--- lib/presentation/login_screen.dart
+++ lib/presentation/login_screen.dart
@@ -11,30 +11,37 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  /// boolean to check whether the state is login or not
   bool isLogin=false;
   @override
   Widget build(BuildContext context) {
     var height= MediaQuery.of(context).size.height;
     return Scaffold(
       bottomSheet: ClipPath(
+        ///Custom Clipper
         clipper: CustomBottomClip(),
+        /// Using gesture detector to change the  isLogin value
         child: GestureDetector(
           onTap: (){
            setState(() {
              isLogin=true;
            });
           },
+          /// AnimatedContainer to handle animation
           child: AnimatedContainer(
+            /// Duration
             duration:const  Duration(milliseconds: 400),
+            /// Curve
             curve: Curves.decelerate,
             color: Theme.of(context).primaryColor,
+            /// changing the height of bottom sheet with animation using animatedContainer
             height:isLogin?height*0.8: height*0.1,
             width: double.infinity,
             padding:const  EdgeInsets.symmetric(horizontal: 20),
-
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                ///AnimatedContainer to handle animation of size of the container basically height only
                  AnimatedContainer(
                    duration: const Duration(milliseconds: 400),
                  height:isLogin?100: 50,
@@ -42,6 +49,7 @@ class _LoginScreenState extends State<LoginScreen> {
                  child: TextUtil(text: "Login",size: 30,),
                ),
                 Expanded(
+                 /// Using Custom Animated ShowUpAnimated  to handle slide animation of textfield
                   child:isLogin? ShowUpAnimation(
                     delay: 200,
                     child: Padding(
@@ -49,6 +57,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
+                          /// Custom FieldWidget
                           const   FieldWidget(title: "Email",icon: Icons.mail,),
                           const  FieldWidget(title: "Password",icon: Icons.key,),
                           const   SizedBox(height: 20,),
@@ -76,6 +85,7 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Column(
         children: [
           ClipPath(
+            ///Custom Clipper
             clipper:CustomUpClip(),
             child: Container(
               padding: const EdgeInsets.all(20),
@@ -86,6 +96,7 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               alignment: Alignment.center,
               child: InkWell(
+                /// Using Ink well to change the  isLogin value
                 onTap: (){
                   setState(() {
                     isLogin=false;
@@ -100,6 +111,7 @@ class _LoginScreenState extends State<LoginScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                /// Custom FieldWidget
                const   FieldWidget(title: "Email",icon: Icons.mail,),
                 const  FieldWidget(title: "Password",icon: Icons.key,),
                 const  FieldWidget(title: "Confirm Password",icon: Icons.key,),
